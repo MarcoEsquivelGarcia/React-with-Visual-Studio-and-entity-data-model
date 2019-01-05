f99b12c0bc1e4e547dc0cbe3c778eea1d6d2b2e4
Marco Esquivel <maesquivelga@gmail.com>
2019-01-05 03:53:48 -0600

    my web api

---
+++ MyStoreServicio(WebAPI)/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyStoreServicio_WebAPI_.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MyStoreServicio(WebAPI)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyStoreServicio_WebAPI_.WebApiApplication" Language="C#" %>
